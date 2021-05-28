815c042ddf49b89f51608473aa9484660590d9cb
Vitaly Turovsky <vital2580@icloud.com>
2021-05-28 07:52:51 +0300

    feat: type `Object.keys`

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,7 @@
 type StringKeys<T extends object> = Extract<keyof T, string>;
 
 interface ObjectConstructor {
+    keys<T extends object>(obj: T): (keyof T)[];
     entries<T extends object>(obj: T): [keyof T, T[keyof T]][];
     // todo review https://stackoverflow.com/questions/57390305/trying-to-get-fromentries-type-right
     fromEntries<T extends [string, any][]>(obj: T): Record<T[number][0], T[number][1]>;
