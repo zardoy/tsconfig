c7f8fb49a57940c4ed75252f9c8c0287747cf362
Vitaly Turovsky <vital2580@icloud.com>
2021-07-03 01:03:11 +0300

    fix: Object.keys and Object.entries always return strigified keys

--- index.d.ts
+++ index.d.ts
@@ -3,8 +3,8 @@
 type StringKeys<T extends object> = Extract<keyof T, string>;
 
 interface ObjectConstructor {
-    keys<T extends object>(obj: T): (keyof T)[];
-    entries<T extends object>(obj: T): [keyof T, T[keyof T]][];
+    keys<T extends object>(obj: T): (StringKeys<T>)[];
+    entries<T extends object>(obj: T): [StringKeys<T>, T[keyof T]][];
     // todo review https://stackoverflow.com/questions/57390305/trying-to-get-fromentries-type-right
     fromEntries<T extends [string, any][]>(obj: T): Record<T[number][0], T[number][1]>;
 }
