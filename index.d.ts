ca38ca3b69f1b882eea7118b6e52a839a24adf44
Vitaly Turovsky <vital2580@icloud.com>
2022-03-21 07:13:43 +0300

    style: format

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
 // make types are actually strict
 
-type StringKeys<T extends object> = Extract<keyof T, string>;
+type StringKeys<T extends object> = Extract<keyof T, string>
 
 interface ObjectConstructor {
-    keys<T extends object>(obj: T): (StringKeys<T>)[];
-    entries<T extends object>(obj: T): [StringKeys<T>, T[keyof T]][];
+    keys<T extends object>(obj: T): StringKeys<T>[]
+    entries<T extends object>(obj: T): [StringKeys<T>, T[keyof T]][]
     // todo review https://stackoverflow.com/questions/57390305/trying-to-get-fromentries-type-right
-    fromEntries<T extends [string, any][]>(obj: T): Record<T[number][0], T[number][1]>;
-    assign<T extends Record<string, any>, K extends Record<string, any>>(target: T, source: K): asserts target is T & K;
+    fromEntries<T extends [string, any][]>(obj: T): Record<T[number][0], T[number][1]>
+    assign<T extends Record<string, any>, K extends Record<string, any>>(target: T, source: K): asserts target is T & K
 }
